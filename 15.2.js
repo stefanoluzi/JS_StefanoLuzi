c11deafa594c67425a9561dd5b0fd4811d2503a5
stefanoluzi <stefanoluzi@hotmail.com>
2023-02-20 22:20:29 -0300

    Segunda clase

--- 15.2.js
+++ 15.2.js
@@ -1,3 +1,33 @@
 let edad = 30;
 
 console.log(edad);
+
+// VARIABLES
+let nombreDeUsuario= "stefano"
+const apellido= "Luzi"
+
+console.log(nombreDeUsuario)
+
+// Puedo renombrar
+nombreDeUsuario="Jorge"
+console.log(nombreDeUsuario)
+
+console.log(apellido)
+
+// No puedo renombrar CONST
+// apellido="Gonzalez"
+
+//ANCHOR BOOLEANO
+
+let estaLogueado = true
+
+let email
+
+//ANCHOR CONCATENACIÓN
+
+let nombre="juan"
+let surname="lopez"
+
+console.log(`hola como estas ${nombre} ${surname}`)
+
+
